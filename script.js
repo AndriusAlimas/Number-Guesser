101b7fac3867e5fe42ecb9c0c44ceb2c824d4408
Andrius <alimasWebDeveloper@gmail.com>
2021-02-20 16:58:36 +0000

    Major update to script.js
    generateTarget() function has a bug should return value. Another function created compareGuesses() - Determines which player (human or computer) wins based on which guess is closest to the target.

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ let currentRoundNumber = 1;
 // This function will be called at the start of each new round in order to generate the new secret target number.
 const generateTarget = () =>{
     // This function should return a random integer between 0 and 9.
-    target = Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (userGuess,computerGuess,secretTarget) =>{
+    /* Determines which player (human or computer) wins based on which guess is closest to the target.
+     If both players are tied, the human user should win.
+     */
+
+    // convert string to integers
+    userGuess = parseInt(userGuess);
+    computerGuess = parseInt(computerGuess);
+    secretTarget = parseInt(secretTarget);
+
+    // check who is closest to target and make sure its positive number
+    let userClose = Math.abs(secretTarget - userGuess);
+    let computerClose = Math.abs(secretTarget - computerGuess);
+  
+    // if user closest then return true otherwise false
+    (userClose > computerClose)? true : false;
 }
