2b2968c75af613315a1d610b1e8ee8f2a2219c93
Andrius <alimasWebDeveloper@gmail.com>
2021-02-20 16:32:07 +0000

    Update script.js
    Created function generateTarget() where computer generate number from 0 to 9, and user have to guess this number

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// This function will be called at the start of each new round in order to generate the new secret target number.
+const generateTarget = () =>{
+    // This function should return a random integer between 0 and 9.
+    target = Math.floor(Math.random() * 10);
+}
